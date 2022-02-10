9e978dadb5be865898908f6f93dd391c74155217
AAncapo <98633424+AAncapo@users.noreply.github.com>
2022-02-10 15:44:07 -0500

    added class rival

---
+++ PokemonGrumps/Rival.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGrumps
+{
+    class Rival
+    {
+        public string Name { get; set; }
+        public List<Pokemon> rivalPKMNteam = new List<Pokemon>();
+
+        public Rival(string rivalName)
+        {
+            Name = rivalName;
+        }
+
+        public Rival()
+        {
+
+        }
+
+        public void _AddPKMNteam(Pokemon PKMN)
+        {
+            rivalPKMNteam.Add(PKMN);
+        }
+    }
+}
