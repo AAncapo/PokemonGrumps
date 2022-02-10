1c983e7725f94c6bd63ac59ab50fb828197bc24b
AAncapo <98633424+AAncapo@users.noreply.github.com>
2022-02-10 17:37:47 -0500

    added array for store trainers names

--- PokemonGrumps/Rival.cs
+++ PokemonGrumps/Rival.cs
@@ -8,8 +8,10 @@ namespace PokemonGrumps
 {
     class Rival
     {
+        Random randNum = new Random();
         public string Name { get; set; }
         public List<Pokemon> rivalPKMNteam = new List<Pokemon>();
+
 
         public Rival(string rivalName)
         {
@@ -21,11 +23,16 @@ namespace PokemonGrumps
 
         }
 
+        void setRandomName()
+        {
+            string[] names = { "Gravedigger Ted", "Dan", "Arin" };
+            int totalNames = names.GetLength(0);
+            Name = names[randNum.Next(0, totalNames)];
+        }
+
         public void _AddPKMNteam(Pokemon PKMN)
         {
             rivalPKMNteam.Add(PKMN);
         }
-
-
     }
 }
