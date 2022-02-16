305784d86db1e46a96ee513d099edcd8e1fa1ee4
AAncapo <98633424+AAncapo@users.noreply.github.com>
2022-02-15 19:40:28 -0500

    added move.txt and a Move constructor

---
+++ PokemonGrumps/Move.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.IO;
+
+namespace PokemonGrumps
+{
+    class Move
+    {
+        public string Tag;
+        int Type;
+        int Pow;
+        string[] moveType = { "NORMAL", "FIRE", "WATER", "GRASS", "ELECTRIC", "ICE", "FIGHT", "POISON", "GROUND", "FLYING", "PSYCHIC", "BUG", "ROCK", "GHOST", "DRAGON", "DARK", "STEEL" };
+        public static string[,] movelist = new string[15, 3];
+
+        public Move(int moveId)
+        {
+            Tag = movelist[moveId, 0].ToUpper();
+            Type = int.Parse(movelist[moveId, 1]);
+            Pow = int.Parse(movelist[moveId, 2]);
+        }
+
+        public void displayMoveDetails()
+        {
+            WriteLine($"========Move Details========\n> {Tag} - {moveType[Type]} - Pow. {Pow}\nEffect:\n============================\n");
+        }
+        
+        public static void Moves()
+        {
+            string moves = "moves.txt";
+            string lines = File.ReadAllText(moves);
+            string[] data = lines.Split('.');
+            int z = 0;
+            for (int y = 0; y < movelist.GetLength(0); y++)
+            {
+                for (int x = 0; x < movelist.GetLength(1); x++)
+                {
+                    movelist[y, x] = data[z];
+                    z++;
+                }
+            }
+        }
+    }
+}
